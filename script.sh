c8b89b8b60f579cbf4664ba83db893884aa752c1
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-02-07 09:47:23 -0300

    script_echo

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo '$@' #agregando el arroba toma todo los argumentos, en cambio si yo pongo 1, por ejemplo, me toma solo el primer argumento
+echo "$@" #agregando el arroba toma todo los argumentos, en cambio si yo pongo 1, por ejemplo, me toma solo el primer argumento
 node -v
